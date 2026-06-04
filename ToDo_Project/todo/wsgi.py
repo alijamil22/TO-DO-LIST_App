89d90a46b9823781f6bfaeb50154975d2ad8d4a4
Ali Jamil <alijamil223344@gmail.com>
2026-06-05 03:07:57 +0500

    Modify WSGI config to include sys.path
    Removed docstring and added sys.path modification.

--- ToDo_Project/todo/wsgi.py
+++ ToDo_Project/todo/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for todo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
