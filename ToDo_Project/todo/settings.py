8ec45a9e1b925d8e249e1017e27bacde45e28b03
alijamil22 <alijamil2223344@gmail.com>
2026-06-05 02:48:52 +0500

    Tweak the allowed hosts settings

--- ToDo_Project/todo/settings.py
+++ ToDo_Project/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+uwfv%+ft30udsb2h-2_xt^zdpu2l^rd6p^x+2=eg*0(yoykmf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
